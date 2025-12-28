f1d310360fd3e0c7dbab58794ff96f95a2986a20
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-12-28 17:40:56 +0800

    feat: webp

--- js/index.js
+++ js/index.js
@@ -1,29 +1,32 @@
 import "./sensitive-content.js";
 
 function addScriptTag(src, async, onload) {
-  var scriptElement = document.createElement("script");
-  scriptElement.async = async;
-  scriptElement.src = src;
-  if (onload) {
-    scriptElement.onload = onload;
-  }
-  var firstScriptElement = document.getElementsByTagName("script")[0];
-  firstScriptElement.parentNode.insertBefore(scriptElement, firstScriptElement);
+    var scriptElement = document.createElement("script");
+    scriptElement.async = async;
+    scriptElement.src = src;
+    if (onload) {
+        scriptElement.onload = onload;
+    }
+    var firstScriptElement = document.getElementsByTagName("script")[0];
+    firstScriptElement.parentNode.insertBefore(
+        scriptElement,
+        firstScriptElement,
+    );
 }
 
 addScriptTag(
-  "https://unpkg.com/save-ukraine@0.18.111/dist/umd/main.js",
-  true,
-  function () {
-    Ukraine.save({
-      ribbon: "TOP_LEFT",
-      countries: [],
-      hasShadow: true,
-      isCancelable: true,
-      isBloodIncluded: false,
-      isGraphicIncluded: false,
-      isInConsole: false,
-      moreInfoUrl: "https://war.ukraine.ua/",
-    });
-  }
+    "https://unpkg.com/save-ukraine@0.18.111/dist/umd/main.js",
+    true,
+    function () {
+        Ukraine.save({
+            ribbon: "TOP_LEFT",
+            countries: [],
+            hasShadow: true,
+            isCancelable: true,
+            isBloodIncluded: false,
+            isGraphicIncluded: false,
+            isInConsole: false,
+            moreInfoUrl: "https://war.ukraine.ua/",
+        });
+    },
 );
