f1c4557c5c40ef90288fcc4cc3adf9b794eea71e
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-12-30 11:39:19 +0800

    feat: canvas-confetti

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import grayscale from "./grayscale.js";
+import confetti from "canvas-confetti";
 
 // Run a function safely
 function safeRun(func) {
@@ -121,3 +122,48 @@ safeRun(function () {
 
 // Grayscale
 safeRun(grayscale);
+
+// Confetti
+safeRunAwait(async function () {
+    const avatar = document.querySelector(".avatar");
+    if (avatar) {
+        avatar.addEventListener("click", function () {
+            var count = 200;
+            var defaults = {
+                origin: { y: 0.7 },
+            };
+
+            function fire(particleRatio, opts) {
+                confetti({
+                    ...defaults,
+                    ...opts,
+                    particleCount: Math.floor(count * particleRatio),
+                    disableForReducedMotion: true,
+                });
+            }
+
+            fire(0.25, {
+                spread: 26,
+                startVelocity: 55,
+            });
+            fire(0.2, {
+                spread: 60,
+            });
+            fire(0.35, {
+                spread: 100,
+                decay: 0.91,
+                scalar: 0.8,
+            });
+            fire(0.1, {
+                spread: 120,
+                startVelocity: 25,
+                decay: 0.92,
+                scalar: 1.2,
+            });
+            fire(0.1, {
+                spread: 120,
+                startVelocity: 45,
+            });
+        });
+    }
+});
